f452ddd303f1c94276e1993d7a83fc6a3ae10b0f
bahar <baharhek@yahoo.com>
2021-04-23 06:58:33 +0430

    meo

--- maArray.js
+++ maArray.js
@@ -8,21 +8,40 @@
 // }
 // console.log(maxi([ 6, 2, 1, 8, 10 ]));
 
-function sumArray(array){
-    let hi = array.toString().split(" ");
-    const bahar=hi.map((hi)=>parseInt(hi));
-    let maria=bahar.reduce((bahar)=>(value1 > maxi) ? value1 : max);
-    let maryam=bahar.reduce((bahar)=>(value2 < mini) ? value2 : min);
-    console.log(maria);
-    console.log(maryam);
-    let all= maria + maryam ;
-    let arsalan = bahar.reduce((accumulator, currentValue) => accumulator + currentValue);
-    let sara = all - arsalan ;
-    console.log(sara);
+// function sumArray(array){
+//     let hi = array.toString().split(" ");
+//     const bahar=hi.map((hi)=>parseInt(hi));
+//     let maria=bahar.reduce((bahar)=>(value1 > maxi) ? value1 : max);
+//     let maryam=bahar.reduce((bahar)=>(value2 < mini) ? value2 : min);
+//     console.log(maria);
+//     console.log(maryam);
+//     let all= maria + maryam ;
+//     let arsalan = bahar.reduce((accumulator, currentValue) => accumulator + currentValue);
+//     let sara = all - arsalan ;
+//     console.log(sara);
+// }
+// sumArray([ 6, 2, 1, 8, 10 ]);
+
+// function digitize(n){
+//    let bahar = n.toString().trim().split("");
+//    const bahar1=bahar.map(n => parseInt(n));
+//    let fatemeh = bahar1.reverse();
+//    console.log(fatemeh);
+// }
+// console.log(digitize(35231));
+
+function countPositivesSumNegatives(input) {
+  let nahar= input.map( (input) => (summer) );
+  for( obb of input){
+    if(obb>0){ count++;}
+    let counter=count;
+  
+  let summer=obb.reduce((accumulator, currentValue) => accumulator + currentValue )
+  }
+    return nahar;
 }
-sumArray([ 6, 2, 1, 8, 10 ]);
 
-
+    console.log( countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]));
 
 
 
